e8ff66d92a6777e941bbe9ebddb0b566829bf13a
Гуминский Виктор Максимович <23626@dune.edu>
2025-09-17 12:09:49 +0200

    Ничего не изменено, просто коммит...

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+//комент1
+//комент2
+//комент3
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
